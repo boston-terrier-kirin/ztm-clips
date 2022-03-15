38f641fe1ee64fdebba8f5884d9a11fafe25ca9a
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-15 16:48:07 +0900

    Section7-108 InputComponent form.get('name')を使うとAbstractFormが取れてしまうため、InputComponent.controlと型が一致しない問題を解決。

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ModalComponent } from './modal/modal.component';
+import { InputComponent } from './input/input.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { TabContainerComponent } from './tab-container/tab-container.component';
 import { TabComponent } from './tab/tab.component';
 
 @NgModule({
-  declarations: [ModalComponent, TabContainerComponent, TabComponent],
-  imports: [CommonModule],
-  exports: [ModalComponent, TabContainerComponent, TabComponent],
+  declarations: [
+    ModalComponent,
+    TabContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
+  imports: [CommonModule, ReactiveFormsModule],
+  exports: [
+    ModalComponent,
+    TabContainerComponent,
+    TabComponent,
+    InputComponent,
+  ],
 })
 export class SharedModule {}
---
+++ src/app/user/register/register.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css'],
+})
+export class RegisterComponent implements OnInit {
+  name = new FormControl('', [Validators.required, Validators.minLength(3)]);
+  email = new FormControl('');
+  age = new FormControl('');
+  password = new FormControl('');
+  confirmPassword = new FormControl('');
+  phoneNumber = new FormControl('');
+
+  form = new FormGroup({
+    name: this.name,
+    email: this.email,
+    age: this.age,
+    password: this.password,
+    confirmPassword: this.confirmPassword,
+    phoneNumber: this.phoneNumber,
+  });
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
