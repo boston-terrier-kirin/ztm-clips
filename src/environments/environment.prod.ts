0d76aadaf15488c5c726a9d07c9ceb5728126ef0
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-16 11:17:05 +0900

    Section10-152：@angular/fireの初期設定

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCTl0TSIZhx-WwjQjXko5sNNT0BcRVkn-0',
+    authDomain: 'ztm-clips.firebaseapp.com',
+    projectId: 'ztm-clips',
+    storageBucket: 'ztm-clips.appspot.com',
+    appId: '1:126923389803:web:55d584e76648cf7ee36a2e',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCTl0TSIZhx-WwjQjXko5sNNT0BcRVkn-0',
+    authDomain: 'ztm-clips.firebaseapp.com',
+    projectId: 'ztm-clips',
+    storageBucket: 'ztm-clips.appspot.com',
+    appId: '1:126923389803:web:55d584e76648cf7ee36a2e',
+  },
 };
 
 /*
