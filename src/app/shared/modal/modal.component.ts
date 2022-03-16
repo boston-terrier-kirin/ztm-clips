94f2317b342eeaf4b74a165caeab99cffa6c6358
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-16 16:09:23 +0900

    Section10-168 ・app-modalをbody直下に配置しているので、手動で消しに行かないとapp-auth-modalが残ったままになる。 ・app.component.htmlで*ngIfしたことで、app-auth-modalが自動で閉じる。

--- src/app/shared/modal/modal.component.ts
+++ src/app/shared/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, Input, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnDestroy, OnInit } from '@angular/core';
 import { ModalService } from 'src/app/services/modal.service';
 
 @Component({
@@ -6,7 +6,7 @@ import { ModalService } from 'src/app/services/modal.service';
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy {
   @Input() modalId = '';
 
   constructor(
@@ -17,6 +17,11 @@ export class ModalComponent implements OnInit {
   ngOnInit(): void {
     // これでapp-modalがbody直下にくるので、親要素のCSS引き継ぎ問題を解決できる。
     document.body.appendChild(this.elementRef.nativeElement);
+  }
+
+  ngOnDestroy(): void {
+    // app-modalをbody直下に配置しているので、手動で消しに行かないとapp-auth-modalが残ったままになる。
+    document.body.removeChild(this.elementRef.nativeElement);
   }
 
   closeModal() {
