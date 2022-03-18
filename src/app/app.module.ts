baaa8364307cc059d9c6cfdd93a65a3a0e35d540
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-19 08:00:06 +0900

    Section12-185 routerLinkActiveは部分一致なので[routerLinkActiveOptions]="{ exact: true }"と一緒に使う。

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,11 @@ import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
 import { environment } from 'src/environments/environment';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, AboutComponent],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebase),
