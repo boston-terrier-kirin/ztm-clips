ba79849d8191af64ae6d28cbf8c7e491a5867ee7
boston-terrier-kirin <kohei.matsumoto@gmail.com>
2022-03-19 09:08:35 +0900

    Section12-191 ログアウトをクリックしたら必ずログインページに戻るわけではないので、 router.eventsを使って、ログインページに遷移させるかを判定する。

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -4,9 +4,9 @@ import {
   AngularFirestore,
   AngularFirestoreCollection,
 } from '@angular/fire/compat/firestore';
-import { Router } from '@angular/router';
-import { Observable } from 'rxjs';
-import { map, delay } from 'rxjs/operators';
+import { ActivatedRoute, NavigationEnd, Router } from '@angular/router';
+import { Observable, of } from 'rxjs';
+import { map, delay, filter, switchMap } from 'rxjs/operators';
 import User from '../models/User';
 
 @Injectable({
@@ -16,15 +16,27 @@ export class AuthService {
   private usersCollection: AngularFirestoreCollection<Partial<User>>;
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
+  private redirect = false;
 
   constructor(
     private auth: AngularFireAuth,
     private firestore: AngularFirestore,
-    private router: Router
+    private router: Router,
+    private route: ActivatedRoute
   ) {
     this.usersCollection = this.firestore.collection('users');
     this.isAuthenticated$ = this.auth.user.pipe(map((user) => !!user));
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000));
+
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        map((event) => this.route.firstChild),
+        switchMap((route) => route?.data ?? of({}))
+      )
+      .subscribe((data) => {
+        this.redirect = data['authOnly'] ?? false;
+      });
   }
 
   public async createUser(userData: User) {
@@ -57,6 +69,9 @@ export class AuthService {
     }
 
     await this.auth.signOut();
-    await this.router.navigateByUrl('/');
+
+    if (this.redirect) {
+      await this.router.navigateByUrl('/');
+    }
   }
 }
